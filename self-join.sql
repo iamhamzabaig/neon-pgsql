3f85d25da7a414451490aa739af470d6089ffff6
iamhamzabaig <hamzamunawar345@gmail.com>
2026-06-23 16:14:25 +0500

    Refactor self-join.sql to include additional SELECT statements with LEFT JOIN and INNER JOIN examples

--- self-join.sql
+++ self-join.sql
@@ -17,3 +17,20 @@ VALUES
   (8, 'Salley', 'Lester', 3);
 
 SELECT * FROM employee;
+
+SELECT
+  e.first_name || ' ' || e.last_name employee,
+  m.first_name || ' ' || m.last_name manager
+FROM
+  employee e
+  LEFT JOIN employee m ON m.employee_id = e.manager_id
+ORDER BY
+  manager;
+
+  
+SELECT f1.title,
+    f2.title,
+    f1.length
+FROM film f1
+    INNER JOIN film f2 ON f1.film_id > f2.film_id
+    AND f1.length = f2.length;
