89085fefe5608000c86f15f859646a4df680557e
iamhamzabaig <hamzamunawar345@gmail.com>
2026-06-23 16:51:35 +0500

    Add natural-join.sql with examples of NATURAL JOIN and NATURAL LEFT JOIN queries

---
+++ natural-join.sql
@@ -0,0 +1,45 @@
+CREATE TABLE categories (
+  category_id SERIAL PRIMARY KEY,
+  category_name VARCHAR (255) NOT NULL
+);
+
+CREATE TABLE products (
+  product_id serial PRIMARY KEY,
+  product_name VARCHAR (255) NOT NULL,
+  category_id INT NOT NULL,
+  FOREIGN KEY (category_id) REFERENCES categories (category_id)
+);
+
+INSERT INTO categories (category_name)
+VALUES
+  ('Smartphone'),
+  ('Laptop'),
+  ('Tablet'),
+  ('VR')
+RETURNING *;
+
+INSERT INTO products (product_name, category_id)
+VALUES
+  ('iPhone', 1),
+  ('Samsung Galaxy', 1),
+  ('HP Elite', 2),
+  ('Lenovo Thinkpad', 2),
+  ('iPad', 3),
+  ('Kindle Fire', 3)
+RETURNING *;
+
+
+SELECT *
+FROM products
+NATURAL JOIN categories;
+
+
+
+SELECT *
+FROM categories
+NATURAL LEFT JOIN products;
+
+
+SELECT *
+FROM city
+NATURAL JOIN country;
